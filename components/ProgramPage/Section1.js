ee004118577210950f605e44183dd8e2a9152d68
oemuraye <oemuraye@gmail.com>
2025-01-20 05:18:30 +0100

    section editing

--- components/ProgramPage/Section1.js
+++ components/ProgramPage/Section1.js
@@ -13,39 +13,42 @@ const Section1 = () => {
         </article>
 
         <section className="program-list">
-            <div className="program-item padding_y-spacing">
-                <div className="row">
-                    <div className="col-md-5 col-sm-12 mb-5 position-relative">
-                        <div className="image_container">
-                            <Image src={programImage1} alt="Program 1" className='img-fluid' height="462px"/>
-                        </div>
+    <div className="program-item-wrapper">
+        <div className="program-item padding_y-spacing">
+            <div className="row">
+                <div className="col-md-5 col-sm-12 mb-5 position-relative">
+                    <div className="image_container">
+                        <Image src={programImage1} alt="Program 1" className='img-fluid' height="462px"/>
                     </div>
-                    <div className="col-md-7 col-sm-12 mb-5 program-details rounded-4 p-2 p-md-4">
-                        <div className="p-md-4 p-2 py-5">
-                            <h3>KARATU GIRLS:</h3>
-                            <h5>Unlocking the Future for Secondary School Girls</h5>
-                            <p>At MemaTech, we believe in creating tailored opportunities that empower girls, women, and small businesses to thrive in a tech-driven world. Through our two core initiatives—Karatu Girls and Masma Reach—we address the unique needs of young girls and women, equipping them with the skills, confidence, and opportunities to excel in technology and beyond.</p>
-                        </div>
+                </div>
+                <div className="col-md-7 col-sm-12 mb-5 program-details rounded-4 p-2 p-md-4">
+                    <div className="p-md-4 p-2 py-5 details-text">
+                        <h3>KARATU GIRLS:</h3>
+                        <h5>Unlocking the Future for Secondary School Girls</h5>
+                        <p>At MemaTech, we believe in creating tailored opportunities that empower girls, women, and small businesses to thrive in a tech-driven world. Through our two core initiatives—Karatu Girls and Masma Reach—we address the unique needs of young girls and women, equipping them with the skills, confidence, and opportunities to excel in technology and beyond.</p>
                     </div>
                 </div>
             </div>
-            <div className="program-item padding_y-spacing">
-                <div className="row">
-                    <div className="col-md-5 col-sm-12 mb-5 position-relative">
-                        <div className="image_container">
-                            <Image src={programImage1} alt="Program 1" className='img-fluid' height="462px"/>
-                        </div>
+        </div>
+        <div className="program-item padding_y-spacing">
+            <div className="row">
+                <div className="col-md-5 col-sm-12 mb-5 position-relative">
+                    <div className="image_container">
+                        <Image src={programImage1} alt="Program 1" className='img-fluid' height="462px"/>
                     </div>
-                    <div className="col-md-7 col-sm-12 mb-5 program-details rounded-4 p-2 p-md-4 basic-bg_color">
-                        <div className="p-md-4 p-2 py-5">
-                            <h3>KARATU GIRLS:</h3>
-                            <h5>Unlocking the Future for Secondary School Girls</h5>
-                            <p>At MemaTech, we believe in creating tailored opportunities that empower girls, women, and small businesses to thrive in a tech-driven world. Through our two core initiatives—Karatu Girls and Masma Reach—we address the unique needs of young girls and women, equipping them with the skills, confidence, and opportunities to excel in technology and beyond.</p>
-                        </div>
+                </div>
+                <div className="col-md-7 col-sm-12 mb-5 program-details rounded-4 p-2 p-md-4 basic-bg_color">
+                    <div className="p-md-4 p-2 py-5 ">
+                        <h3>KARATU GIRLS:</h3>
+                        <h5>Unlocking the Future for Secondary School Girls</h5>
+                        <p>At MemaTech, we believe in creating tailored opportunities that empower girls, women, and small businesses to thrive in a tech-driven world. Through our two core initiatives—Karatu Girls and Masma Reach—we address the unique needs of young girls and women, equipping them with the skills, confidence, and opportunities to excel in technology and beyond.</p>
                     </div>
                 </div>
             </div>
-        </section>
+        </div>
+    </div>
+</section>
+
     </section>
   )
 }
