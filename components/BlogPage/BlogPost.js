7c00c22de2246e2848ec5a9fbad34104866f1ed8
oemuraye <oemuraye@gmail.com>
2025-01-24 06:23:46 +0100

    blog post edit

--- components/BlogPage/BlogPost.js
+++ components/BlogPage/BlogPost.js
@@ -10,10 +10,10 @@ const BlogPost = ({ blogPost }) => {
             </button>
             <div className="d-flex justify-content-center">
                 <Image src={blogPost.image} alt={blogPost.title} className="img-fluid" style={{ maxHeight: "320px" }}/>
-                <h1 className="mt-4 text-center">{blogPost.title}</h1>
-                {/* <p className="mt-3">{blogPost.content}</p> */}
-                <div className="mt-3" dangerouslySetInnerHTML={{ __html: blogPost.content }}></div>
             </div>
+            <h1 className="mt-4 text-center">{blogPost.title}</h1>
+            {/* <p className="mt-3">{blogPost.content}</p> */}
+            <div className="mt-3" dangerouslySetInnerHTML={{ __html: blogPost.content }}></div>
           </div>
     </section>
   )
